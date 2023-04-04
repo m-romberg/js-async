45974378176fbfdff7532a7f51a0956c11738b9d
Alyssa Denary <alyssa.denary@gmail.com>
2023-04-03 20:18:35 -0400

    clean up comments

--- numbers/numbers.js
+++ numbers/numbers.js
@@ -4,22 +4,14 @@ const $resultsBatchDiv = $("#number-batch-results");
 const $favNumDiv = $("#number-fav-results");
 const NUMBERS_URL = "http://numbersapi.com/";
 
-// make the request
-
-//pull data from response
-
-// turn into html elements
-
-//append to dom
-
-async function get_multiple_nums_fact(){
+async function get_multiple_nums_fact() {
   const resp = await axios.get(`${NUMBERS_URL}31,33,90?json`);
   const nums_fact = resp.data;
 
   const $batchNumHeading = $(`<h2>"Facts on multiple numbers"</h2>`);
   $resultsBatchDiv.append($batchNumHeading);
 
-  for (let num in nums_fact){
+  for (let num in nums_fact) {
     const $fact_html = $(`<p id="fact-${num}">${nums_fact[num]}</p>`);
     $resultsBatchDiv.append($fact_html);
   }
@@ -27,12 +19,9 @@ async function get_multiple_nums_fact(){
 
 get_multiple_nums_fact();
 
-// make 4 requests order doesn't matter, but wait until we have them all
-//  allSettled return an array of promises
-//  #QUESTION: How to catch/resend request if any of your promises fails?
-//  #QUESTION: diff when to use all vs allSettled - gives reason for failure, can get other results
+//  #QUESTION: How to catch/resend request if any of your promises fails? Try/catch
 
-async function get_facts_fav_num(){
+async function get_facts_fav_num() {
   const fact1P = axios(`${NUMBERS_URL}33`);
   const fact2P = axios(`${NUMBERS_URL}33`);
   const fact3P = axios(`${NUMBERS_URL}33`);
@@ -43,7 +32,7 @@ async function get_facts_fav_num(){
   const $favNumHeading = $(`<h2>"Facts on my Fav num"</h2>`);
   $favNumDiv.append($favNumHeading);
 
-  for (const p of results){
+  for (const p of results) {
     const $favNumEl = $(`<p>${p.value.data}</p>`);
     $favNumDiv.append($favNumEl);
   }
